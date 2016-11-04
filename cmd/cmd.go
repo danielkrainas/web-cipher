5ae5896b4b3f9ede375cab4179c93e2eee399b79
Danny Krainas <dkrainas87@gmail.com>
2016-11-03 23:45:15 -0400

    add flag support in cmd abstraction

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -13,6 +13,20 @@ type Info struct {
 	Short string
 	Long  string
 	Run   ExecutorFunc
+	Flags []*Flag
+}
+
+type FlagType string
+
+var (
+	FlagString FlagType = "string"
+)
+
+type Flag struct {
+	Short       string
+	Long        string
+	Description string
+	Type        FlagType
 }
 
 var registry map[string]*Info = make(map[string]*Info)
@@ -33,8 +47,9 @@ func CreateDispatcher(ctx context.Context, info *Info) func() error {
 	}
 }
 
-func makeCobraRunner(ctx context.Context, innerFunc ExecutorFunc) func(cmd *cobra.Command, args []string) error {
+func makeCobraRunner(ctx context.Context, innerFunc ExecutorFunc, flags []*Flag) func(cmd *cobra.Command, args []string) error {
 	return func(cmd *cobra.Command, args []string) error {
+		ctx = contextWithFlags(ctx, cmd, flags)
 		return innerFunc(ctx, args)
 	}
 }
@@ -46,9 +61,31 @@ func makeCobraCommand(ctx context.Context, info *Info) *cobra.Command {
 		Long:  info.Long,
 	}
 
+	for _, f := range info.Flags {
+		switch f.Type {
+		case FlagString:
+			cmd.PersistentFlags().StringP(f.Long, f.Short, "", "")
+		}
+
+	}
+
 	if info.Run != nil {
-		cmd.RunE = makeCobraRunner(ctx, info.Run)
+		cmd.RunE = makeCobraRunner(ctx, info.Run, info.Flags)
 	}
 
 	return cmd
+}
+
+func contextWithFlags(ctx context.Context, cmd *cobra.Command, flags []*Flag) context.Context {
+	if len(flags) < 1 {
+		return ctx
+	}
+
+	values := make(map[string]interface{})
+	for _, f := range flags {
+		v := cmd.PersistentFlags().Lookup(f.Long).Value.String()
+		values["flags."+f.Long] = v
+	}
+
+	return context.WithValues(ctx, values)
 }
